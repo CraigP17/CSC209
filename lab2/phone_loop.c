cb616bea8bde8ba1f5b5190af6c7b61d491a609c
Craig Pinto <craigpinto17@gmail.com>
2020-01-16 14:08:55 -0500

    Updating phone_loop bug

--- lab2/phone_loop.c
+++ lab2/phone_loop.c
@@ -5,19 +5,23 @@ int main() {
   char phone[11];
   int index;
   int scan = 0;
+  int wrong = 0;
   scanf("%s", phone);
 
-  do {
+  while(scan != EOF) {
     scan = scanf("%d", &index);
     if (index == -1) {
         printf("%s\n", phone);
     } else if ((0 <= index)&&(index <= 9)) {
         printf("%c\n", phone[index]);
     } else {
+      wrong = 1;
       printf("ERROR\n");
-      return 1;
     }
-  } while(scan != EOF);
+  }
 
+  if (wrong) {
+    return 1;
+  }
   return 0;
 }
