f00bb35539a7e7e8a42da8e667db899998e630c0
Markus <markus@markus.com>
2020-02-10 23:03:41 -0500

    Update starter code files for assignment lab6

---
+++ lab6/copy.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Write the copy function to perform exactly as strncpy does, with one
+   exception: your copy function will guarantee that dest is always
+   null-terminated.
+   You shoud read the man page to learn how strncpy works.
+
+  NOTE: You must write this function without using any string functions.
+  The only function that should depend on string.h is memset.
+ */
+
+char *copy(char *dest, const char *src, int capacity) {
+
+
+    return dest;
+}
+
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: copy size src\n");
+        exit(1);
+    }
+    int size = strtol(argv[1], NULL, 10);
+    char *src = argv[2];
+
+    char dest[size];
+    memset(dest, 'x', size);
+
+    copy(dest, src, size);
+
+    printf("%s\n", dest);
+    return 0;
+}
