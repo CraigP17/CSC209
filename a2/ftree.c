6fc5118b67f78678a56093ee75292d1a45871974
Craig Pinto <craigpinto17@gmail.com>
2020-02-12 16:39:21 -0500

    Commenting and Cleaning

--- a2/ftree.c
+++ a2/ftree.c
@@ -15,24 +15,28 @@
  * Returns a pointer to the heap
  */
 struct TreeNode *maker(const char *fnamer, char* path) {
+    // Malloc space for TreeNode
     struct TreeNode *nodes = malloc(sizeof(struct TreeNode));
     if (nodes == NULL) {
       fprintf(stderr, "Could not malloc space\n");
       exit(1);
     }
 
+    // Get info about the file
     struct stat file;
     if (lstat(path, &file) == -1) {
       printf("Cant lstat\n");
     }
 
+    // Malloc space for TreeNode->fname char*
     nodes->fname = malloc(sizeof(char*)*strlen(fnamer));
     if (nodes->fname == NULL) {
       fprintf(stderr, "Could not malloc space\n");
       exit(1);
     }
-    strcpy(nodes->fname, fnamer);
 
+    // Fill in TreeNode data for the File
+    strcpy(nodes->fname, fnamer);
     nodes->permissions = file.st_mode & 0777;
     nodes->contents = NULL;
     nodes->next = NULL;
@@ -58,13 +62,15 @@ struct TreeNode *make_structs(const char *fnames, char* path) {
     struct TreeNode *current = NULL;
     struct TreeNode *next = NULL;
 
+    // Create char* of the path to the file, include the filename
     int length = strlen(path) + strlen(fnames) + 2;
     char curr_path[length];
     strcpy(curr_path, path);
     strcat(curr_path, fnames);
     lstat(curr_path, &input);
 
-    // Is a File
+    // Is a File or Link, create a TreeNode using the current path to it
+    // Return that Tree Node. (Base Case)
     if (S_ISREG(input.st_mode)) {
       mains = maker(fnames, curr_path);
       return mains;
@@ -72,26 +78,38 @@ struct TreeNode *make_structs(const char *fnames, char* path) {
       mains = maker(fnames, curr_path);
       return mains;
     } else if (S_ISDIR(input.st_mode)) {
+      // When it is a directory:
+      // Create a TreeNode for the root directory
       mains = maker(fnames, curr_path);
+
+      // Open the Directory
       DIR *directory = opendir(curr_path);
       if (directory == NULL) {
         fprintf(stderr, "Error opening directory: %s\n", curr_path);
         exit(1);
       }
+
+      // Read the directory, first file, filter out an . or ..
       struct dirent *dir_file;
       dir_file = readdir(directory);
       while ((dir_file != NULL)&&((dir_file->d_name)[0] == '.')) {
         dir_file = readdir(directory);
       }
 
+      // Create a TreeNode for first link of file in Directory
+      // Assigned to directory contents
       strcat(curr_path, "/");
       mains->contents = make_structs(dir_file->d_name, curr_path);
       current = mains->contents;
+
+      // Read directory for the next file, check if . or ..
       dir_file = readdir(directory);
       while ((dir_file != NULL)&&((dir_file->d_name)[0] == '.')) {
         dir_file = readdir(directory);
       }
 
+      // Loop through directory making TreeNodes for each file,
+      // Connect the file TreeNodes as a linked list
       while (dir_file != NULL) {
         next = make_structs(dir_file->d_name, curr_path);
         current->next = next;
@@ -101,7 +119,10 @@ struct TreeNode *make_structs(const char *fnames, char* path) {
           dir_file = readdir(directory);
         }
       }
-      closedir(directory);
+
+      if (closedir(directory) == -1) {
+        fprintf(stderr, "Could not close directory\n");
+      }
     }
     return mains;
 }
@@ -151,6 +172,9 @@ void print_ftree(struct TreeNode *root) {
     // printf("%*s", depth * 2, "");
 
     // Your implementation here.
+
+    // If file or link, print the root information
+    // Check if file/links are connected to more files, via root->next
     if (root->type == '-') {
       printf("%*s", depth * 2, "");
       printf("%s (%c%o)\n", root->fname, root->type, root->permissions);
@@ -164,12 +188,15 @@ void print_ftree(struct TreeNode *root) {
         print_ftree(root->next);
       }
     } else if (root->type == 'd'){
+      // When it is a direcotry, check the contents and recursively print
       printf("%*s", depth * 2, "");
       printf("===== %s (%c%o) =====\n", root->fname, root->type, root->permissions);
+      // Increase depth space when entering a directory to print contents
       depth++;
       if (root->contents != NULL) {
         print_ftree(root->contents);
       }
+      // Decrease depth after finishing printing directory to connect with root
       depth--;
       if (root->next != NULL) {
         print_ftree(root->next);
@@ -186,6 +213,8 @@ void print_ftree(struct TreeNode *root) {
 void deallocate_ftree (struct TreeNode *node) {
 
    // Your implementation here.
+   // If file or link, free the fname that was malloced and the the root
+   // TreeNode that was malloced
    if (node->type == '-') {
      free(node->fname);
      free(node);
@@ -193,6 +222,9 @@ void deallocate_ftree (struct TreeNode *node) {
      free(node->fname);
      free(node);
    } else {
+     // If it is a directory, if contents
+     // Similarly by lopping through linked list, loop through file links
+     // to free each TreeNode and name
      if (node->contents != NULL) {
        struct TreeNode *curr = node->contents;
        struct TreeNode *root = node->contents;
@@ -208,6 +240,7 @@ void deallocate_ftree (struct TreeNode *node) {
        free(node->fname);
        free(node);
      } else {
+       // If empty directory
        free(node->fname);
        free(node);
      }
