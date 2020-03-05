ad754aba0165a785a2c07c4a72c5b6862c2511a2
Markus <markus@markus.com>
2020-03-05 12:57:20 -0500

    Update starter code files for assignment lab8

---
+++ lab8/checkpasswd.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAXLINE 256
+
+#define SUCCESS "Password verified\n"
+#define INVALID "Invalid password\n"
+#define NO_USER "No such user\n"
+
+int main(void) {
+  char user_id[MAXLINE];
+  char password[MAXLINE];
+
+  /* The user will type in a user name on one line followed by a password 
+     on the next.
+     DO NOT add any prompts.  The only output of this program will be one 
+	 of the messages defined above.
+   */
+
+  if(fgets(user_id, MAXLINE, stdin) == NULL) {
+      perror("fgets");
+      exit(1);
+  }
+  if(fgets(password, MAXLINE, stdin) == NULL) {
+      perror("fgets");
+      exit(1);
+  }
+  
+  // TODO
+
+  return 0;
+}
