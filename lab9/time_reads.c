d175d63871796f048bb4942e991590c1b37a835f
Markus <markus@markus.com>
2020-03-13 15:33:08 -0400

    Update starter code files for assignment lab9

---
+++ lab9/time_reads.c
@@ -0,0 +1,47 @@
+/* The purpose of this program is to practice writing signal handling
+ * functions and observing the behaviour of signals.
+ */
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+/* Message to print in the signal handling function. */
+#define MESSAGE "%ld reads were done in %ld seconds.\n"
+
+/* Global variables to store number of read operations and seconds elapsed. 
+ */
+long num_reads, seconds;
+
+
+/* The first command-line argument is the number of seconds to set a timer to run.
+ * The second argument is the name of a binary file containing 100 ints.
+ * Assume both of these arguments are correct.
+ */
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: time_reads s filename\n");
+        exit(1);
+    }
+    seconds = strtol(argv[1], NULL, 10);
+
+    FILE *fp;
+    if ((fp = fopen(argv[2], "r")) == NULL) {
+      perror("fopen");
+      exit(1);
+    }
+
+    /* In an infinite loop, read an int from a random location in the file,
+     * and print it to stderr.
+     */
+    for (;;) {
+
+
+
+
+    }
+    return 1; // something is wrong if we ever get here!
+}
