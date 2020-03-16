3dbb893b88518a02acb125a91f227b82e5f1105b
Craig Pinto <craigpinto17@gmail.com>
2020-03-16 15:46:58 -0400

    Added error check wrappers

--- a3/parallel_closest.c
+++ a3/parallel_closest.c
@@ -9,6 +9,31 @@
 #include "serial_closest.h"
 #include "utilities_closest.h"
 
+// fork() function to create child processes with built error checking
+int fork_wrap() {
+  int res;
+  if ((res = fork()) == -1) {
+    perror("fork");
+    exit(1);
+  }
+  return res;
+}
+
+// pipe() function for communication via pipe with built error checking
+void pipe_wrap(int *fd) {
+  if (pipe(fd) == -1) {
+    perror("pipe");
+    exit(1);
+  }
+}
+
+// close() function to close a pipe end with built error checking
+void close_wrap(int fd) {
+  if (close(fd) == -1) {
+    perror("Close");
+    exit(1);
+  }
+}
 
 /*
  * Multi-process (parallel) implementation of the recursive divide-and-conquer
@@ -27,27 +52,18 @@ double closest_parallel(struct Point *p, int n, int pdmax, int *pcount) {
 
     // Create a pipe for child 1 to communicate with the parent
     int pipe_1[2];
-    if (pipe(pipe_1) == -1) {
-      perror("pipe 1");
-      exit(1);
-    }
+    pipe_wrap(pipe_1);
 
     // Forking first child for left distances
-    int child1 = fork();
-    if (child1 < 0) {
-      perror("fork");
-      exit(1);
-    } else if (child1 == 0) {
+    int child1 = fork_wrap();
+    if (child1 == 0) {
       // Child Process for Finding Distance
       double left_min;
       left_min = closest_parallel(p, mid, pdmax - 1, pcount);
 
       // Child writes to Parent about Left Distance through Pipe
       // Child writes so close reading end
-      if (close(pipe_1[0]) == -1) {
-        perror("Close Child Read End");
-        exit(1);
-      }
+      close_wrap(pipe_1[0]);
 
       // Child now writes the value in pipe
       if (write(pipe_1[1], &left_min, sizeof(double)) != sizeof(double)) {
@@ -56,10 +72,7 @@ double closest_parallel(struct Point *p, int n, int pdmax, int *pcount) {
       }
 
       // Close Write End
-      if (close(pipe_1[1]) == -1) {
-        perror("Close Child Write End");
-        exit(1);
-      }
+      close_wrap(pipe_1[1]);
 
       // Exit with number of worker processes
       exit(*pcount + 1);
@@ -69,27 +82,18 @@ double closest_parallel(struct Point *p, int n, int pdmax, int *pcount) {
 
       // Create a Pipe for Second Child
       int pipe_2[2];
-      if (pipe(pipe_2) == -1) {
-        perror("pipe 2");
-        exit(1);
-      }
+      pipe_wrap(pipe_2);
 
       // Forking second child for right distances
-      int child2 = fork();
-      if (child2 < 0) {
-        perror("fork 2");
-        exit(1);
-      } else if (child2 == 0) {
+      int child2 = fork_wrap();
+      if (child2 == 0) {
         // Child 2 Process for Finding Distance
         double right_min;
-        right_min = closest_parallel(&p[mid], n - mid, pdmax - 1, pcount);
+        right_min = closest_parallel(p + mid, n - mid, pdmax - 1, pcount);
 
         // Child writes to Parent about Right Distance through Pipe
         // Child writes so close reading end
-        if (close(pipe_2[0]) == -1) {
-          perror("Close Child 2 Read End");
-          exit(1);
-        }
+        close_wrap(pipe_2[0]);
 
         // Child now writes the value in pipe
         if (write(pipe_2[1], &right_min, sizeof(double)) != sizeof(double)) {
@@ -98,10 +102,7 @@ double closest_parallel(struct Point *p, int n, int pdmax, int *pcount) {
         }
 
         // Close Write End
-        if (close(pipe_2[1]) == -1) {
-          perror("Close Child 2 Write End");
-          exit(1);
-        }
+        close_wrap(pipe_2[1]);
 
         // Exit with number of worker processes + 1 for current
         exit(*pcount + 1);
@@ -133,36 +134,26 @@ double closest_parallel(struct Point *p, int n, int pdmax, int *pcount) {
         double min_left, min_right;
 
         // Close Write from Child 1 Pipe
-        if (close(pipe_1[1]) == -1) {
-          perror("Close read from Child 1");
-          exit(1);
-        }
+        close_wrap(pipe_1[1]);
         // Read from Child 1 Pipe
         if (read(pipe_1[0], &min_left, sizeof(double)) != sizeof(double)) {
           perror("Read from Child 1 Pipe");
           exit(1);
         }
         // Close read from Child 1 Pipe
-        if (close(pipe_1[0]) == -1) {
-          perror("Close read from Child 1");
-          exit(1);
-        }
+        close_wrap(pipe_1[0]);
+
 
         // Close Write from Child 2 Pipe
-        if (close(pipe_2[1]) == -1) {
-          perror("Close read from Child 2");
-          exit(1);
-        }
+        close_wrap(pipe_2[1]);
         // Read from Child 2 Pipe
         if (read(pipe_2[0], &min_right, sizeof(double)) != sizeof(double)) {
           perror("Read from Child 2 Pipe");
           exit(1);
         }
         // Close read from Child 2 Pipe
-        if (close(pipe_2[0]) == -1) {
-          perror("Close read from Child 2");
-          exit(1);
-        }
+        close_wrap(pipe_2[0]);
+
 
         // Find minimum of distance of two points,
         // one from left points and one from right points
